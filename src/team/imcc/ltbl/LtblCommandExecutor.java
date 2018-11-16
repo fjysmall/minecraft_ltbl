59aa2a74af65c7cbd8d7ed9564ae0453db135b6a
Jingyi Feng <fengjingyi1122@gmail.com>
2018-11-16 22:22:19 +0800

    add block util to check mob swpan

--- src/team/imcc/ltbl/LtblCommandExecutor.java
+++ src/team/imcc/ltbl/LtblCommandExecutor.java
@@ -1,7 +1,7 @@
 package team.imcc.ltbl;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -44,16 +44,44 @@ public class LtblCommandExecutor implements CommandExecutor {
 
                             @Override
                             public void run() {
-                                Location loc = player.getLocation();
-                                loc.setY(loc.getY() + 2);
 
-                                // default FLAME
-                                ParticleUtil.sendParticle(player, loc, 26, 0, 0);
+                                if (!player.isOnline()) {
+                                    player2Task.remove(player.getName());
+                                    this.cancel();
+                                    return;
+                                }
+
+                                int radius = 64;
+                                for (int i = -radius; i <= radius; i++) {
+                                    for (int j = -radius; j <= radius; j++) {
+                                        for (int k = -radius; k <= radius; k++) {
+
+                                            Location location = player.getLocation();
+                                            location.setX(location.getX() + i);
+                                            location.setY(location.getY() + j);
+                                            location.setZ(location.getZ() + k);
+
+                                            Block block = player.getWorld().getBlockAt(location);
+                                            if (!BlockUtil.isSpawnableBlock(block)) {
+                                               continue;
+                                            }
+
+                                            if (block.getLightFromBlocks() <= 7) {
+                                                Location displayLocation = block.getLocation();
+                                                displayLocation.setX(displayLocation.getX() + 0.5);
+                                                displayLocation.setY(displayLocation.getY() + 0.1);
+                                                displayLocation.setZ(displayLocation.getZ() + 0.5);
+                                                // default FLAME 26
+                                                ParticleUtil.sendParticle(player, displayLocation, 26, 0, 0);
+                                            }
+                                        }
+                                    }
+                                }
                             }
                         }
                     );
 
-                    this.player2Task.get(player.getName()).runTaskTimerAsynchronously(this.plugin, 0, 10);
+                    this.player2Task.get(player.getName()).runTaskTimerAsynchronously(this.plugin, 16, 64);
                 }
 
                 return true;
